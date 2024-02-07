2d03b7e5b5dccc9efcb12463db7f75e6e7184f98
allan-kirui <allan.kipkemoi.kirui@gmail.com>
2024-02-07 19:24:17 +0100

    different filters for images

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -24,6 +24,16 @@ def load_image(imagePath):
 
     return [pixls[i:i+img.width] for i in range(0, len(pixls), img.width)]
 
+def rgb_average(rgbTuple):
+    average = sum(rgbTuple) / len(rgbTuple)
+    return average
+
+def rgb_to_lightness(rgbTuple):
+    return (max(rgbTuple) + min(rgbTuple))/2
+
+def rgb_to_luminosity(rgbTuple):
+    return 0.21 * rgbTuple[0] + 0.72 * rgbTuple[1] + 0.07 * rgbTuple[2]
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     imageDir = Path("images")
@@ -32,10 +42,13 @@ if __name__ == '__main__':
     pathToImage = get_image_path(imageDir, imageFileName)
 
     pixels = load_image(pathToImage)
-
+    brightnessMatrix = []
     print("Iterating through pixels (R,G,B): ")
     for x in range(len(pixels)):
+        brightnessRow = []
         for y in range(len(pixels[x])):
-            pixel = pixels[x][y]
-            print(pixel)
+            rowList = list(map(rgb_average, pixels[x]))
+        # append list to 2d list
+        brightnessMatrix.append(rowList)
 
+    print(brightnessMatrix)
